d06b6d78e8ba02e60088af0aec7d488f410e1640
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-04 09:56:29 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
@@ -6,11 +6,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    Button btn;
+
 }
