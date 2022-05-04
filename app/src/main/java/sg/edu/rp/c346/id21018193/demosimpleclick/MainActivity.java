ddd8640bcdb3f92bc39e2c31d037b5f916b11fe2
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-04 10:02:53 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
@@ -2,7 +2,9 @@ package sg.edu.rp.c346.id21018193.demosimpleclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Handle button click event here
+                Toast.makeText(MainActivity.this, "The Button has been clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
