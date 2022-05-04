cdef9c721a5578f23b351b7d48570f333ff791a5
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-04 09:58:07 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demosimpleclick/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn1);
+
     }
 
 
